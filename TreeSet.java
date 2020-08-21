f05f99334172e0f023028acad0f6d69c15265046
Shivani-1998 <69883266+Shivani-1998@users.noreply.github.com>
2020-08-21 14:26:53 +0530

    v1

---
+++ TreeSet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+  public class TreeSet {
+  public static void main(String[] args) {
+     TreeSet<String> treeset = new TreeSet<String>();
+          treeset.add("Red");
+          treeset.add("Green");
+          treeset.add("Black");
+          treeset.add("Pink");
+          treeset.add("orange");
+     System.out.println("Original tree set: " + treeset);
+    System.out.println("Size of the tree set: " + treeset.size());
+   }
+}
