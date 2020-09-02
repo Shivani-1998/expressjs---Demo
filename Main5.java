f24ff3ba8b4cddcef3226762d933c32dc164cf2a
Shivani-1998 <69883266+Shivani-1998@users.noreply.github.com>
2020-09-02 09:05:48 +0530

    Create public access modifier file

---
+++ Main5.java
@@ -0,0 +1,16 @@
+//publlic Access Modifier.
+class A 
+{ 
+   public void display() 
+      { 
+          System.out.println("Public Access Modifier"); 
+      } 
+} 
+class Main5
+{ 
+    public static void main(String args[]) 
+      { 
+          A obj = new A (); 
+          obj.display(); 
+      } 
+}
